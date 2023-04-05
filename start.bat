6b3d8186cb69806a531987cefef2115f336b9074
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2023-04-05 16:17:31 +0100

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-pip install opencv-python
-pip install PyAutoGUI
-pip install numpy
-copy main.py c:/zoui/main.py
-echo del c:\zoui\s.txt\npython3 c:/zoui/main.py > "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\s.bat"
-echo 1 > c:/zoui/s.txt
-python3 c:/zoui/main.py
