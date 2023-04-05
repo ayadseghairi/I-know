d854206b549b85b725f7a7e0d5ed9157aa47435a
Ayad seghairi <85361584+ayadseghairi@users.noreply.github.com>
2023-04-05 16:12:27 +0100

    Add files via upload

---
+++ stop_or_start.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+set "file=c:\zoui\s.txt"
+set /p value=<"%file%"
+if !value! equ 0 (
+  set "new_value=1"
+  python3 c:/zoui/main.py
+) else (
+  set "new_value=0"
+)
+echo !new_value!>"%file%"
+
